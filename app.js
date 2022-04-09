95aef572afc3939d213b5f43230bd30b5991212d
EricN <eric_nakagawa@yahoo.com>
2022-04-09 01:36:51 -0700

    Promises exercise done

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let watching = new Promise ((resolve, reject) => {
+     let userWatching = Math.round(Math.random())
+
+
+if(userWatching){
+     resolve("Thumbs up and Sub")
+}else {
+     reject("User left")
+}
+})
+
+
+watching
+     .then ((value) => {
+          console.log(value)
+     })
+     .catch((error) => {
+          console.log(error)
+     })
